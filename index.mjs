5ac303052a805cdc5cebb39ca73aee590ef3200f
Ryan DeVault <69012106+CtrlVaultDel@users.noreply.github.com>
2023-08-27 14:25:13 -0500

    narrowed down habitible planets

--- index.mjs
+++ index.mjs
@@ -3,22 +3,33 @@ import fs from 'fs';
 
 const results = [];
 
+const parser = parse({
+    columns: true,
+    delimiter: ',',
+    comment: '#'
+})
+
+function isHabitible(planet){
+    return planet?.koi_disposition === 'CONFIRMED' 
+    && planet?.koi_insol > 0.36 
+    && planet?.koi_insol < 1.11 
+    && planet?.koi_prad < 1.6;
+}
+
 fs.createReadStream('./data/kepler_data.csv')
     // .pipe connects readable stream source to writable stream destination
-    .pipe(parse({
-        columns: true,
-        delimiter: ',',
-        comment: '#'
-    }))
-    .on('data', (data) => {
-        results.push(data)
+    .pipe(parser)
+    .on('data', (planet) => {
+        if(isHabitible(planet)){
+            results.push(planet)
+        }
     })
     .on('error', (error) => {
         console.error(error)
     })
     .on('end', () => {
-        console.log('results', results[0])
-        console.log('Finished reading file')
+        console.log('results', results)
+        console.log(`Potentially habitible planets: ${results.length}`)
     })
 
 
